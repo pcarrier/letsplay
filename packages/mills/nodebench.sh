cf85a57b7455cdb86a9531b62d98441c5d5ae170
Pierre Carrier <pierre@gcarrier.fr>
2015-12-04 07:40:16 -0800

    publish nodebench.sh

---
+++ packages/mills/nodebench.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm _hydrogen.cfg _code.asm
+
+set -ex
+
+node \
+  --trace-hydrogen \
+  --trace-hydrogen-file=_hydrogen.cfg \
+  --trace-phase=Z \
+  --trace-deopt \
+  --code-comments \
+  --hydrogen-track-positions \
+  --redirect-code-traces \
+  --redirect-code-traces-to=_code.asm \
+  --print-opt-code \
+  nodebench.js "$@"
